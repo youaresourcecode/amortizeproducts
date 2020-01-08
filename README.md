9866f8e51cb02f6f1d4abdfa0666e8e8ee2682ee
youaresourcecode <youaresourcecode@protonmail.com>
2020-01-08 15:31:23 +0100

    refactor and loop over products

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ Email used: dsv46090@zzrgg.com
 2) For loop
 3) Mostrar la información por meses
 4) Mostrar la información por meses
+5) Moment.js?
 
 ##Time spent
 60 minutes
--- js/script.js
+++ js/script.js
@@ -3,9 +3,6 @@ function init(){
   var products = [];
   var producto1 = {};
   producto1.id = '1';
-  //producto1.nombre = 'macbookpro2019';
-  //producto1.fechaDeCompra = new Date(2020, 00, 07);//macbookpro2019
-  //producto1.precio = '2800';
   producto1.nombre = 'iphone11';
   producto1.fechaDeCompra = new Date(2019, 11, 01);//iphone11
   producto1.hoy = new Date();
@@ -19,25 +16,24 @@ function init(){
   producto2.precio = '2800';
 	producto2.dias = 0;
 
-	const oneDay = 24 * 60 * 60 * 1000; //h*min*sec*mil (mil en 1 dia)
-/*
-  for(let i = 0; i<products.length; i++){
-  	console.log('Calculando datos para producto ' + i);	
-  }
-*/
-	producto1.dias = Math.round(Math.abs((producto1.fechaDeCompra-producto1.hoy) / oneDay));	
-  var cadaDia = producto1.dias/producto1.precio;
-  console.log("El producto " + producto1.nombre + " se compró hace " + producto1.dias + " días y te ha costado " +  	 parseFloat(cadaDia).toFixed(8) + '€ por día'); 
-  var p = document.createElement('p');
-  p.id = "idZonaProductos";
-  p.innerHTML = "El producto " + producto1.nombre + " se compró hace " + producto1.dias + " días y te ha costado " +  	 parseFloat(cadaDia).toFixed(8) + '€ por día'; 
-  document.getElementById("idProductos").appendChild(p);
+  products.push(producto1);
+  products.push(producto2);
+  //Un ejemplo de un producto que no sirve para esta web:
+  //Pago 1 año de gimnasio con 365 días a 160 -> 160/365 = 0,43€ el día de gimnasio
 
-	producto2.dias = Math.round(Math.abs((producto2.fechaDeCompra-producto2.hoy) / oneDay));	
-  var cadaDia = producto2.dias/producto2.precio;
-  console.log("El producto " + producto2.nombre + " se compró hace " + producto2.dias + " días y te ha costado " +  	 parseFloat(cadaDia).toFixed(8) + '€ por día'); 
-  var p = document.createElement('p');
-  p.id = "idZonaProductos";
-  p.innerHTML = "El producto " + producto2.nombre + " se compró hace " + producto2.dias + " días y te ha costado " +  	 parseFloat(cadaDia).toFixed(8) + '€ por día'; 
-  document.getElementById("idProductos").appendChild(p);
+	const oneDay = 24 * 60 * 60 * 1000; //h*min*sec*mil (mil en 1 dia)
+  for(let i = 0; i<products.length; i++){
+    muestraAmortizacionProducto(products[i]);
+  }
+
+  function muestraAmortizacionProducto(p)
+  {
+    p.dias = Math.round(Math.abs((p.fechaDeCompra-p.hoy) / oneDay));	
+    var cadaDia = p.precio/p.dias;
+//console.log("El producto " + p.nombre + " se compró hace " + p.dias + " días y te ha costado " + parseFloat(cadaDia).toFixed(8) + '€ por día'); 
+    var productoAInsertar = document.createElement('p');
+    productoAInsertar.id = "idZonaProductos";
+    productoAInsertar.innerHTML = "El producto " + p.nombre + " se compró hace " + p.dias + " días y te ha costado " + parseFloat(cadaDia).toFixed(8) + '€ por día'; 
+    document.getElementById("idProductos").appendChild(productoAInsertar);
+  }
 }
